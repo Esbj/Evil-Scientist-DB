39d2a2ab6c0769b86c57213a5350faf24b4e9be4
Alexander <alex.esbj@gmail.com>
2023-01-13 12:43:28 +0100

    start of input gathering

--- src/script.ts
+++ src/script.ts
@@ -2,18 +2,18 @@ class Scientist {
   protected name: string;
   protected age: number;
   protected numberOfHenchmen: number;
-  protected image: string;
+  protected image?: string;
   protected desc?: string;
 
   constructor(
     name: string,
     age: number,
     numberOfHenchmen: number,
-    image: string,
+    image?: string,
     desc?: string
   ) {
-    this.name = name
-    this.age = age
+    this.name = name;
+    this.age = age;
     this.numberOfHenchmen = numberOfHenchmen;
     this.image = image;
     this.desc = desc;
@@ -38,15 +38,26 @@ const doofenshmirtz = new Scientist(
   "images/Doofenshmirtz.png",
   "Dr. Doofenshmirtz is a scientist whos main focus on mechanics that has a high output of machines, mainly ray guns with evil intent."
 );
-
 const drakken = new Scientist("Dr. Drakken", 45, 1, "images/Dr.Drakken.png");
 
 db.addScientist(drakken);
 db.addScientist(doofenshmirtz);
 
-document.querySelector("input[submit]")?.addEventListener("submit", (event) => event.preventDefault() viewEditor.addScientist())
-class viewEditor {
-  addScientist():void{
-    let inputs = document.querySelectorAll("input[text], input[textarea]");
+let viewEditor = {
+  listScientist(): void {
+    // let scientistHolder = document.querySelector("#list-scientists");
+  },
+  addScientist(): void {
+    const formName = document.querySelector("input#name") as HTMLInputElement
+    const formAge = document.querySelector("input#age") as HTMLInputElement
+    const formHenchmen = document.querySelector("input#henchmen") as HTMLInputElement
+    const formDesc = document.querySelector("input#desc") as HTMLInputElement
   }
-}
+};
+
+const addButton = document.querySelector("button");
+addButton?.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("click");
+  viewEditor.addScientist()
+});
